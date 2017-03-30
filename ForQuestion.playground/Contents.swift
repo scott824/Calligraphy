6ef14757ccd152331a89c853d5e8e02f0e32486a
Sang Chul Lee <scott824@naver.com>
2017-03-30 18:18:10 +0900

    Answer

--- ForQuestion.playground/Contents.swift
+++ ForQuestion.playground/Contents.swift
@@ -8,7 +8,12 @@ var n = 10
 for i in 1...n {
     sum += i;
 }
-print (sum);
+
+/* answer */
+var sumArray = Array(1...n)
+var resultSum = sumArray.reduce(0, {$0 + $1})
+
+print(sum, resultSum);
 
 //Q2
 var fibo1 = 0, fibo2 = 1, result = 0
@@ -19,7 +24,14 @@ for i in 1 ... n-2 {
     fibo1 = fibo2
     fibo2 = result
 }
-print(result)
+
+/* answer */
+var fiboArray = Array(1...n-2)
+var resultFibo = fiboArray.reduce((0, 1)) { (ret: (Int, Int), index: Int) -> (Int, Int) in
+    return (ret.1, ret.0 + ret.1)
+}.1
+
+print(result, resultFibo)
 
 //Q3
 var fact = 1
@@ -29,7 +41,11 @@ for i in 1 ... n {
     fact *= i
 }
 
-print(fact)
+/* answer */
+var array = Array(1 ... n)
+var resultFact = array.reduce(1, { $0 * $1 })
+
+print(fact, resultFact)
 
 //Q4
 var prime:[Int] = []
@@ -40,7 +56,13 @@ for i in 2 ... n {
         prime.append(i);
     }
 }
-print(prime)
+
+/* answer */
+var numArray = Array(2 ... n)
+numArray = numArray.filter({num in n % num != 0})
+
+
+print(prime, numArray)
 
 //Q5
 let vec1 = [1,2,3], vec2 = [4,5,6]
@@ -48,4 +70,6 @@ result = 0
 for i in 0 ... 2{
     result += vec1[i] * vec2[i]
 }
-print(result)
+var resultVec = vec1.enumerated().map({ (offset, num) in num * vec2[offset]}).reduce(0, {$0 + $1})
+
+print(result, resultVec)
